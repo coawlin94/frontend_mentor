3a052016caf11e3486517fcc58ebefadbe1a824f
coawlin94 <coawlin@hotmail.com>
2019-09-14 23:02:17 +0800

    Complete Project Tracking Intro Component webpage

---
+++ Project Tracking intro component/website/app/js/script.js
@@ -0,0 +1,25 @@
+import '../scss/style.scss';
+
+var menu = document.querySelector(".menu-toggle");
+var menuItems = menu.children;
+var nav = document.querySelector(".nav");
+
+menu.addEventListener("click",function(){
+    
+    for(let acitvated=0; acitvated<menuItems.length; acitvated++)
+    {
+        if(menuItems[acitvated].classList.contains("forward"))
+        {
+            menuItems[acitvated].classList.remove("forward");
+            menuItems[acitvated].classList.add("reverse");
+        }
+        else if(menuItems[acitvated].classList.contains("reverse"))
+        {
+            menuItems[acitvated].classList.remove("reverse");
+            menuItems[acitvated].classList.add("forward");
+        }
+    }
+
+    nav.classList.toggle("active");
+    console.log(nav.classList);
+})
